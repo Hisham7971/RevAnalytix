3c18b994f26b048ebc3a9f4b53b10d8657803cf8
Hisham7971 <mdhisham.dev@gmail.com>
2023-10-09 22:29:59 +0400

    updates

--- appRevAnalytix/urls.py
+++ appRevAnalytix/urls.py
@@ -3,7 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
+    path('', views.index, name='index'),
     path('chat.html/', views.chat_page, name='chat_page'),
-    path('basic-table.html/', views.analyze_sentiment_and_priority, name='analyze_sentiment_and_priority'),
+    path('basic-table.html/', views.basic_table, name='basic_table'),
+    path('datatable.html/', views.datatable, name='datatable'),
+    path('highchart.html/', views.highchart, name='highchart'),
+    path('knob-chart.html/', views.knob_chart, name='knob_chart'),
+    path('apexcharts.html/', views.apexcharts, name='apexcharts'),
 ]
